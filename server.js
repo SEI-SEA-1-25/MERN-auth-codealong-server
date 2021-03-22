9277ad4ed265c48296c1143ed7c01de562b8f7dc
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-22 15:08:57 -0700

    8itu

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
 // require packages
-const express = require('express')
-const rowdy = require('rowdy-logger')
-const morgan = require('morgan')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const rowdy = require("rowdy-logger");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 // required db models -- runs the index file
-require('./models')
+require("./models");
 
 // config express app
-const app = express()
-const PORT = process.env.PORT || 3001
-const rowdyResults = rowdy.begin(app)
+const app = express();
+const PORT = process.env.PORT || 3001;
+const rowdyResults = rowdy.begin(app);
 // middleswares
-app.use(morgan('tiny'))
-app.use(cors())
+app.use(morgan("tiny"));
+app.use(cors());
 // request body parser
 // for html for POSTs
 // app.use(express.urlencoded({ extended: false }))
 // for plain JSON request bodies
-app.use(express.json())
+app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log('hello from a middleware 👋')
-//   next()
-// })
+app.use((req, res, next) => {
+  console.log("hello from a middleware 👋");
+  next();
+});
 
 const middlesware = (req, res, next) => {
-  console.log('hello from a middleware 👋')
-  next()
-}
+  console.log("hello from a middleware 👋");
+  next();
+};
 
 // app.use(middlesware)
 
 // GET / -- test index route
-app.get('/', middlesware, (req, res) => {
-  res.json({ msg: 'hello world 👾' })
-})
+app.get("/", middlesware, (req, res) => {
+  res.json({ msg: "hello world 👾" });
+});
 
 // controllers
-app.use('/api-v1/users', require('./controllers/api-v1/users.js'))
+app.use("/api-v1/users", require("./controllers/api-v1/users.js"));
 
 // tell express to listen on a port
 app.listen(PORT, () => {
-  rowdyResults.print()
-  console.log(`you are listening to the smooth sound of ${PORT} in the morning 🌊`)
-})
+  rowdyResults.print();
+  console.log(
+    `you are listening to the smooth sound of ${PORT} in the morning 🌊`
+  );
+});

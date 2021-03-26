12140ba88f10e3c0454663a69955937ee574bf4e
weston-bailey <wdbailey87@gmail.com>
2021-03-25 20:45:13 -0700

    add procfile

--- models/index.js
+++ models/index.js
@@ -1,26 +1,62 @@
 // require mongoose package
 const mongoose = require('mongoose')
 
-// mongoose config
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/mernAuth'
+// database connection config
+if(process.env.NODE_ENV === 'development') {
+  // mongoose config
+  const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/mernAuth'
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
+  mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
 
-const db = mongoose.connection;
+  const db = mongoose.connection;
 
-// db methods for debug
-db.once('open', () => {
-  console.log(`⛓ mongoDB connection @ ${db.host}:${db.port}`)
-})
+  // db methods for debug
+  db.once('open', () => {
+    console.log(`⛓ mongoDB connection @ ${db.host}:${db.port}`)
+  })
 
-db.on('error', err => {
-  console.error(`🔥 something has gone wrong with the DB!!!!\n ${err}`)
-})
+  db.on('error', err => {
+    console.error(`🔥 something has gone wrong with the DB!!!!\n ${err}`)
+  })
+} else {
+  // mongoDB Atlas code will go here
+  const MongoClient = require('mongodb').MongoClient;
+  
+  const uri = process.env.ATLAS_URI
+  console.log(process.env.ATALS_URI)
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
+
+  // connect mongoose
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+
+  const db = mongoose.connection;
+
+  // db methods for debug
+  db.once('open', () => {
+    console.log(`⛓ mongoDB connection @ ${db.host}:${db.port}`)
+  })
+
+  db.on('error', err => {
+    console.error(`🔥 something has gone wrong with the DB!!!!\n ${err}`)
+  })
+}
+
 // console.dir
 // console.log
 // console == object with function in it
